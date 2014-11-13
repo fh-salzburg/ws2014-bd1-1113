976eb01e739dcaaaa918571ef37fbcf5cc86cb3f
Michael Raidel <raidel@induktiv.at>
2014-11-13 14:17:12 +0100

    answers and questions index & show

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# create questions
+question_1 = Question.create!(title: "first question", body: "lorem ipsum...")
+question_2 = Question.create!(title: "second question", body: "lorem ipsum...")
+
+#create answers
+question_1.answers.create!(body: "first answer to first question")
+question_1.answers.create!(body: "second answer to first question")
+question_2.answers.create!(body: "first answer to second question")
+question_2.answers.create!(body: "second answer to second question")

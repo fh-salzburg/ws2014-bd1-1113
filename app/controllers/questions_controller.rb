2c1bd3027b9478397e590e00971fdc755dcb66b0
Michael Raidel <raidel@induktiv.at>
2014-11-20 09:32:13 +0100

    update a question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,20 @@
 class QuestionsController < ApplicationController
+  before_action :get_question, except: [ :index ]
+
   def index
     @questions = Question.all
   end
 
   def show
+  end
+
+  def update
+    @question.update(params.require(:question).permit(:title, :body))
+    flash[:message] = "gespeichert!"
+    redirect_to @question
+  end
+
+  def get_question
     @question = Question.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # get 'questions' => 'questions#index'
   # get 'questions/:id' => 'questions#show'
 
-  resources :questions, only: [ :index, :show ]
+  resources :questions, only: [ :index, :show, :update ]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
